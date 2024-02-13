2f8d03440780480405d08b5ed85490a25d0f838f
rezanatha <wnrezaldi@gmail.com>
2024-02-14 00:51:58 +0700

    minor changes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const wrapper = document.querySelector('.wrapper');
 const question = document.querySelector('.question');
 const titleImage = document.querySelectorAll('.title-image-div');
+const mainImage = document.querySelector('#main-image');
 const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
 const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 
-var text = "I love you, my little fluffy cat 😘🥰😍🩵🩵🩵";
+var text = "Thank you, my little fluffy cat 😘🥰😍🩵🩵🩵";
 const emojis = ["💗","🧡","🥹","🥰","😘","🫶","❤️","🤍","🐈"]
 
 yesBtn.addEventListener('click', () => {
@@ -16,8 +17,11 @@ yesBtn.addEventListener('click', () => {
         i.innerHTML = "<img src='./img/happy_cat.gif' class='title-image'>";
     });
 
+    mainImage.setAttribute('src', './img/happyvday2.gif')
+
     text += emojis[Math.floor(Math.random() * emojis.length)];
 });
+
 noBtn.addEventListener('click', () => {
     question.innerHTML = "Are you sure?";
 });
